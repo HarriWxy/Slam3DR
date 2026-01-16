00f84a166ac7d6d48d2ec0a259f2cf8c0cdc69f8
HarriW <316361680@qq.com>
2026-01-16 13:51:00 +0000

    添加Local2World模型训练脚本，配置训练和测试数据集，更新损失函数和超参数设置

--- train_3dr.sh
+++ train_3dr.sh
@@ -49,3 +49,28 @@ torchrun --nproc_per_node=2 train.py \
     --loss_func "i2p_corr_score" \
     --output_dir $TRAIN_OUT_DIR \
     --ref_id -1
+
+MODEL="Local2WorldModel(pos_embed='RoPE100', img_size=(224, 224), head_type='linear', output_mode='pts3d', depth_mode=('exp', -inf, inf), conf_mode=('exp', 1, inf), \
+enc_embed_dim=1024, enc_depth=24, enc_num_heads=16, dec_embed_dim=768, dec_depth=12, dec_num_heads=12, \
+mv_dec1='MultiviewDecoderBlock_max',mv_dec2='MultiviewDecoderBlock_max', enc_minibatch = 12, need_encoder=True)"
+
+PRETRAINED="l2w"
+TRAIN_OUT_DIR="checkpoints/slam3r_l2w"
+
+torchrun --nproc_per_node=2 train.py \
+    --train_dataset "${TRAIN_DATASET}" \
+    --test_dataset "${TEST_DATASET}" \
+    --model "$MODEL" \
+    --train_criterion "Jointnorm_ConfLoss(Jointnorm_Regr3D(L21,norm_mode=None), alpha=0.2)" \
+    --test_criterion "Jointnorm_Regr3D(L21, norm_mode=None)" \
+    --pretrained $PRETRAINED \
+    --pretrained_type "dust3r" \
+    --lr 5e-5 --min_lr 5e-7 --warmup_epochs 2 --epochs 20 --batch_size 16 --accum_iter 1 \
+    --save_freq 2 --keep_freq 20 --eval_freq 1 --print_freq 20 --num_workers 16\
+    --save_config\
+    --output_dir $TRAIN_OUT_DIR \
+    --freeze "encoder"\
+    --loss_func "l2w" \
+    --ref_ids 0 1 2 3 4 5
+
+
